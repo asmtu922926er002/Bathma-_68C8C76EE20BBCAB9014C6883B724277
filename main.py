0cf86b939257e8892d6a09b35f1c72a8031e3b9d
asmtu922926er002 <146055444+asmtu922926er002@users.noreply.github.com>
2023-09-26 11:00:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+if num <0:
+print("sorry,factorial does not exist for negative numbers")
+elif num==0:
+print("The Factorial of 0 is 1")
+else:
+for i in range (1,num + 1);
+Foctorial = factorial*i
+print("The factorial of",num,"is",factorial)
